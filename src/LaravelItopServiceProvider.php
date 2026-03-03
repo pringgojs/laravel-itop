372560b207815a63432cd15e5df0bf4b4bc3f13f
Pringgo Juni S <odyinggo@gmail.com>
2026-03-03 08:46:37 +0700

    remove unused code

--- src/LaravelItopServiceProvider.php
+++ src/LaravelItopServiceProvider.php
@@ -11,21 +11,10 @@ class LaravelItopServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/itop.php' => config_path('itop.php'),
         ], 'config');
-
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                Console\Commands\ForwardTestCommand::class,
-            ]);
-        }
     }
 
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/itop.php', 'itop');
-
-        $this->app->singleton('itop.forwarder', function ($app) {
-            return new Services\ForwarderService($app);
-        });
     }
 }
